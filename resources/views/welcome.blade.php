294a3bd939594a9e488f32b12755f9894e035162
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-11-22 17:44:12 +0100

    update 0.01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,21 +109,18 @@
         <div class="container px-4 mx-auto">
             <div class="relative flex flex-wrap items-center -mx-4">
                 <div class="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
-                    <img class="hidden xl:block absolute bottom-0 left-0 -ml-32 -mb-10"
-                        src="zeus-assets/icons/dots/blue-dot-left-bars.svg" alt="">
+
                     <img class="h-96 lg:h-128 w-full rounded-xl object-cover"
-                        src="https://images.unsplash.com/photo-1524508762098-fd966ffb6ef9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+                        src="/img/bg2.jpg"
                         alt="">
                 </div>
                 <div class="relative w-full lg:w-1/2 px-4">
                     <div class="relative lg:pl-10 max-w-lg">
-                        <span class="text-xs text-blue-400 font-semibold">What's new at Shuffle</span>
                         <h2 class="mt-8 mb-6 lg:mb-10 lg:pr-8 text-4xl font-semibold font-heading">Expand your brand with
                             this excellent extension tool</h2>
                         <p class="mb-6 lg:mb-12 text-xl text-gray-500">With this tool, you will get much better results at
                             work and develop new skills. Will you take the risk of trying the latest version of our
                             application?</p>
-                        <a class="text-xl text-red-500 underline hover:no-underline font-medium" href="#">Learn more</a>
                     </div>
                 </div>
             </div>
@@ -362,7 +359,7 @@
                             d="M26.514 25.764L25.292 27.292C23.3984 26.1794 21.8205 24.6015 20.708 22.708L22.236 21.486C22.4151 21.3426 22.5417 21.144 22.596 20.9211C22.6502 20.6982 22.6292 20.4636 22.536 20.254L21.143 17.117C21.0431 16.8922 20.8666 16.7101 20.645 16.6034C20.4234 16.4966 20.1711 16.472 19.933 16.534L17.281 17.221C17.0336 17.2858 16.8186 17.439 16.6765 17.6517C16.5345 17.8643 16.4752 18.1216 16.51 18.375C16.9753 21.6887 18.5068 24.7609 20.8729 27.127C23.2391 29.4932 26.3112 31.0247 29.625 31.49C29.8782 31.5249 30.1355 31.4657 30.348 31.3236C30.5605 31.1815 30.7135 30.9664 30.778 30.719L31.466 28.068C31.5279 27.8299 31.5033 27.5776 31.3966 27.356C31.2898 27.1343 31.1078 26.9579 30.883 26.858L27.746 25.465C27.5364 25.372 27.302 25.351 27.0792 25.405C26.8564 25.4591 26.6577 25.5853 26.514 25.764V25.764Z"
                             stroke="#838EA4" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
-                    <p class="text-gray-500">+213 55 04 04 30</p>
+                    <p class="text-gray-500">+213 555 04 04 30</p>
                 </div>
             </div>
         </div>
@@ -375,22 +372,21 @@
                 <form action="#">
                     <div class="relative flex flex-wrap mb-6">
                         <input class="relative mb-2 md:mb-0 w-full py-4 pl-4 text-sm border rounded" type="text"
-                            placeholder="Daniel">
+                            placeholder="Nom & Prénom">
                         <span
-                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">Name</span>
+                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">Nom</span>
                     </div>
                     <div class="relative flex flex-wrap mb-6">
                         <input class="relative mb-2 md:mb-0 w-full py-4 pl-4 text-sm border rounded" type="email"
                             placeholder="e.g hello@shuffle.dev">
                         <span
-                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">Your
-                            email address</span>
+                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">E-mail</span>
                     </div>
                     <div class="relative flex flex-wrap mb-6">
                         <textarea class="relative mb-2 md:mb-0 w-full py-4 pl-4 text-sm border rounded resize-none" id="1"
                             type="message" cols="30" rows="10" placeholder="Message..."></textarea>
                         <span
-                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">Your
+                            class="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">Votre
                             message</span>
                     </div>
                     <button
@@ -404,8 +400,8 @@
         <div class="container mx-auto px-4">
             <div class="flex flex-wrap justify-between items-center -mx-4">
                 <div class="w-full lg:w-2/6 px-4">
-                    <a class="inline-block mb-6 text-gray-900 text-lg font-semibold" href="#">
-                        <img class="h-7" src="zeus-assets/logo/logo-zeus-red.svg" alt="" width="auto">
+                    <a class="inline-block mb-6 text-gray-900 text-lg font-semibold" href="#accueil">
+                        <img class="h-7" src="img/logo.png" alt="">
                     </a>
                     <p class="hidden lg:block text-sm text-gray-500">Tous droits réservés © Sarl Sobar 2021</p>
                 </div>
@@ -416,7 +412,7 @@
                             <li class="mr-12 mb-2 md:mb-0"><a class="text-sm font-medium" href="#apropos">A propose</a></li>
                             <li class="mr-12 mb-2 md:mb-0"><a class="text-sm font-medium" href="#portfolio">Portfolio</a></li>
                         </ul>
-                        <a class="inline-block mr-auto lg:mr-0 py-4 px-8 text-sm text-white font-medium leafing-normal bg-red-400 hover:bg-red-300 rounded"
+                        <a class="inline-block mr-auto lg:mr-0 py-4 px-8 text-sm font-medium leafing-normal text-black hover:text-white bg-sobar-light  hover:bg-sobar-dark  rounded"
                             href="#contact">Contact</a>
                     </div>
                     <p class="mt-6 lg:hidden text-sm text-gray-500">Tous droits réservés © Sarl Sobar 2021</p>
